c5c1667bcfa51a1e748384d68043d539e464fb8c
Greg Spies <gstotx@hotmail.com>
2018-02-09 14:26:20 -0600

    add rlstats cmd and printCommands method

--- bot.js
+++ bot.js
@@ -1,6 +1,26 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+
+//valid commands
+var commands = [
+    { name: "help",
+      description: "Displays all valid commands and usage information",
+      usage: "!help"
+    },
+    { name: "jeff",
+      description: "My name's Jeff!",
+      usage: "!jeff"
+    },
+    { name: "rlstats",
+      description: "Shows statistics from rocketleague.tracker.network when ID is given",
+      usage: "!rlstats <Steam ID>"
+    }
+
+];
+
+var RL_TRACKER_URL = 'https://rocketleague.tracker.network/profile/steam/';
+var INVALID_ARGS_MSG = 'Invalid arguments. Type !help for more info';
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -26,14 +46,53 @@ bot.on('message', function (user, userID, channelID, message, evt) {
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
+            // !help
+            case 'help':
+                bot.sendMessage({
+                    to: channelID,
+                    message: printCmds(commands)
+                });
+            break;
+
+            // !jeff
             case 'jeff':
                 bot.sendMessage({
                     to: channelID,
                     message: 'My name\'s Jeff!'
                 });
             break;
+            
+            // !stats
+            case 'rlstats':
+                if (args.size() <= 1) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: INVALID_ARGS_MSG 
+                    });
+                }
+                else if (args.size() == 2) {
+                    var accountId = args[1];
+                    bot.sendMessage({
+                    to: channelID,
+                    message: RL_TRACKER_URL + accountId
+                    });
+                }
+            break;
+            
             // Just add any case commands if you want to..
-         }
-     }
+         }     
+    }
 });
+
+function printCmds(cmds){
+    var msg = "";
+    cmds.forEach(function(element) {
+        var keys = Object.keys(element);
+        for(var i = 0; i < keys.length(); i++){
+            msg += keys[i] + ": " + cmd[keys[i]] + "\n";
+        }
+        msg += "\n";
+    });
+
+    return msg;
+}
---
+++ utilities.js
@@ -0,0 +1,13 @@
+/*function printCmds(cmds){
+	var msg = "";
+	cmds.forEach(function(element) {
+		var keys = Object.keys(element);
+		for(var i = 0; i < keys.length(); i++){
+			msg += keys[i] + ": " + cmd[keys[i]] + "\n";
+		}
+		msg += "\n";
+	});
+
+	return msg;
+}
+*/

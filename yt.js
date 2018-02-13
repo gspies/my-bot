7a9ce3b36228e9934f84928f89e3a21ea1619859
Gregory Spies <DZPZV9@NAM.corp.gm.com>
2018-02-13 15:56:39 -0600

    fix package typo

--- yt.js
+++ yt.js
@@ -18,16 +18,24 @@ var name = "finessegreg";
 
 var request = require('request');
 var cheerio = require('cheerio');
-
+var ratings = ["Solo: ", "Doubles: ", "Solo Standard: ", "Standard: "];
 request('http://rltracker.pro/profiles/finessegreg/steam', function (error, response, html) {
   console.log("in request");
   console.log(error);
+  
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var count = 0;
     console.log(html);
     $('div.rating').each(function(i, element){
       var a = $(this).text();
-      console.log(a);
+      if (count < 4){
+      	var a = $(this).text();
+      	ratings[count] += a;
+      }
+      else{
+      	break;
+      }
     });
   }
 });

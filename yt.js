3f2ca033f1d10aad317a7470eacad262d791780a
Gregory Spies <DZPZV9@NAM.corp.gm.com>
2018-02-13 15:42:57 -0600

    try cheerio

--- yt.js
+++ yt.js
@@ -10,18 +10,28 @@ search('rocket league', opts, function(err, results) {
  
   console.dir(results);
 });*/
-var $ = jQuery = require('jquery')(window);
-var http = require('http');
-/*require("jsdom").env("", function(err, window) {
-    if (err) {
-        console.error(err);
-        return;
-    }
- 
-    var $ = require("jquery")(window);
-});*/
 
+/*var $ = jQuery = require('jquery')(window);
+var http = require('http');
 var name = "finessegreg";
-http.get('http://rltracker.pro/profiles/' + name + "/steam", function(response) {
+*/
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+request('http://news.ycombinator.com', function (error, response, html) {
+  console.log("in request");
+  console.log(error);
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html);
+    console.log(html);
+    $('span.comhead').each(function(i, element){
+      var a = $(this).prev();
+      console.log(a.text());
+    });
+  }
+});
+
+/*http.get('http://rltracker.pro/profiles/' + name + "/steam", function(response) {
 	console.log(response);
-})
+})*/

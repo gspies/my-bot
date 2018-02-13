9e725203aeca4f2b3a7d96dbc4d2ed179b7b0f28
Gregory Spies <DZPZV9@NAM.corp.gm.com>
2018-02-13 13:16:47 -0600

    fix yt command

--- bot.js
+++ bot.js
@@ -115,9 +115,9 @@ bot.on('message', mesg => {
                 else{
                     var title = joinArgs(args);
                     search(title, opts, function(err, results) {
-                        console.log(results);
+                        console.log(results[0]);
                         if(err) return console.log(err);
-                        msg = results[0]["link"];    
+                        msg = results[0].link;    
                     });
                 }
                 mesg.reply(msg);

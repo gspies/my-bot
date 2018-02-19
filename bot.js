7373117f3d65cde334e8822fb39af0afb1991621
Gregory Spies <DZPZV9@NAM.corp.gm.com>
2018-02-19 13:05:21 -0600

    refactor rs into method

--- bot.js
+++ bot.js
@@ -195,7 +195,7 @@ bot.on('message', mesg => {
             
             // !rs
             case 'rs':
-                var stats = -1;
+                /*var stats = -1;
                 if (args.size < 1){
                     msg = INVALID_ARGS_MSG;
                 }
@@ -205,8 +205,123 @@ bot.on('message', mesg => {
                     msg = rsStats(url);
                     console.log(msg);
                     mesg.reply(msg);
+                }*/
+                var stats = -1;
+                if (args.size < 1){
+                    msg = INVALID_ARGS_MSG;
                 }
-                
+                else{
+                    var username = joinArgs(args, "%20");
+                    var url = RS_URL + username;
+                    
+
+                    var details = [/* 
+                        {'Rank': 0},
+                        {'Level': 0},
+                        {'XP': 0}
+                    */];
+                    /*var stats = [
+                        {'Overall': {}},
+                        {'Attack': {}},
+                        {'Defense': {}},
+                        {'Strength': {}}/*,
+                        {'Hitpoints': details},
+                        {'Ranged': details},        
+                        {'Prayer': details},
+                        {'Magic': details},
+                        {'Cooking': details},
+                        {'Woodcutting': details},
+                        {'Fletching': details},
+                        {'Fishing': details},
+                        {'Firemaking': details},
+                        {'Crafting': details},
+                        {'Smithing': details},
+                        {'Mining': details},
+                        {'Herblore': details},        
+                        {'Agility': details},
+                        {'Thieving': details},
+                        {'Slayer': details},
+                        {'Farming': details},
+                        {'Runecraft': details},
+                        {'Hunter': details},
+                        'Construction': details}*/
+                    //];
+                    var statNames = [
+                        'Overall',
+                        'Attack',
+                        'Defense',
+                        'Strength',
+                        'Hitpoints',
+                        'Ranged',        
+                        'Prayer',
+                        'Magic',
+                        'Cooking',
+                        'Woodcutting',
+                        'Fletching',
+                        'Fishing',
+                        'Firemaking',
+                        'Crafting',
+                        'Smithing',
+                        'Mining',
+                        'Herblore',        
+                        'Agility',
+                        'Thieving',
+                        'Slayer',
+                        'Farming',
+                        'Runecraft',
+                        'Hunter',
+                        'Construction'
+                    ];
+                    var allStats = [];
+
+                    request(RS_URL, function (error, response, html) {
+                        console.log("in request");
+                        console.log(error); 
+                        if (!error && response.statusCode == 200) {
+                            
+                            var prettyHtml = html.split("\n");
+                            console.log(prettyHtml);
+                            for (var i = 0; i < statNames.length; i++){
+                                var stat = prettyHtml[i].split(',');
+                                var details2 = Object.assign({}, details);
+                                if(stat.length == 3){
+                                    details2['Rank'] = stat[0];
+                                    details2['Level'] = stat[1];
+                                    details2['XP'] = stat[2];
+                                }
+                                allStats[statNames[i]] = details2;
+                            }
+                            
+                        }
+                        
+                        console.log(allStats);
+                        console.log(printStats(allStats));
+
+                        function printStats(allStats){
+                            var msg = "";
+                            for (stat in allStats){
+                                console.log(stat);
+                                msg += stat + "\n"
+                                console.log(allStats[stat]);
+                                msg += "\tRank: " + allStats[stat]['Rank'];
+                                msg += allStats[stat]['Level'];
+                                msg += "\tLevel: " + allStats[stat]['Level'];
+                                msg += allStats[stat]['XP'];
+                                msg += "\tRank: " + allStats[stat]['XP'];
+                                msg += "\n";
+                                
+                            }
+                            
+                            return msg;
+                        }
+
+                        mesg.reply(printStats(allStats));
+                    });
+
+                    //msg = rsStats(url);
+                    //console.log(msg);
+                    
+                }
 
             break;
             

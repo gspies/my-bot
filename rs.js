e0ae204bba7e0fa3faa3968a0e6dd76224558668
Gregory Spies <DZPZV9@NAM.corp.gm.com>
2018-02-19 15:41:52 -0600

    slide in DMs

--- rs.js
+++ rs.js
@@ -1,42 +1,13 @@
-var request = require('request');
+/*var request = require('request');
 var cheerio = require('cheerio');
 
 var RS_URL = 'http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=scoobr';
-/*var html = "1231,3213,123\n" + "142,13,321\n"+ "1421,135,3231\n"+ "142451,13673,32321\n" + "1231,3213,123\n" + "142,13,321\n"+ "1421,135,3231\n"+ "142451,13673,32321\n" +
+var html = "1231,3213,123\n" + "142,13,321\n"+ "1421,135,3231\n"+ "142451,13673,32321\n" + "1231,3213,123\n" + "142,13,321\n"+ "1421,135,3231\n"+ "142451,13673,32321\n" +
 "1231,3213,123\n" + "142,13,321\n"+ "1421,135,3231\n"+ "142451,13673,32321\n" + "1231,3213,123\n" + "142,13,321\n"+ "1421,135,3231\n"+ "142451,13673,32321\n" +
 "1231,3213,123\n" + "142,13,321\n"+ "1421,135,3231\n"+ "142451,13673,32321\n" + "1231,3213,123\n" + "142,13,321\n"+ "1421,135,3231\n"+ "142451,13673,32321";
-*/
-var details = [/* 
-        {'Rank': 0},
-        {'Level': 0},
-        {'XP': 0}
-    */];
-    /*var stats = [
-        {'Overall': {}},
-        {'Attack': {}},
-        {'Defense': {}},
-        {'Strength': {}}/*,
-        {'Hitpoints': details},
-        {'Ranged': details},        
-        {'Prayer': details},
-        {'Magic': details},
-        {'Cooking': details},
-        {'Woodcutting': details},
-        {'Fletching': details},
-        {'Fishing': details},
-        {'Firemaking': details},
-        {'Crafting': details},
-        {'Smithing': details},
-        {'Mining': details},
-        {'Herblore': details},        
-        {'Agility': details},
-        {'Thieving': details},
-        {'Slayer': details},
-        {'Farming': details},
-        {'Runecraft': details},
-        {'Hunter': details},
-        'Construction': details}*/
-    //];
+
+var details = [];
+    
 var statNames = [
     'Overall',
     'Attack',
@@ -106,5 +77,5 @@ request(RS_URL, function (error, response, html) {
         return msg;
     }
 
-});
+});*/
 
